e08b1c4853ab6d478a056161f3a8d91675df7cce
MaratKhabibullinTR <marat.khabibullin@thomsonreuters.com>
2023-09-15 13:36:39 +0300

    artifact url

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 	gcloud auth configure-docker
 
 # tag:
-	docker tag mlflow europe-west3-docker.pkg.dev/a208358-plx-ai-sandbox/ml-training/mlflow:latest
+	docker tag mlflow europe-west3-docker.pkg.dev/a208358-plx-ai-sandbox/ml-training/mlflow:__version__
 
 # push:
-	docker push europe-west3-docker.pkg.dev/a208358-plx-ai-sandbox/ml-training/mlflow:latest
+	docker push europe-west3-docker.pkg.dev/a208358-plx-ai-sandbox/ml-training/mlflow:__version__
--- entry-point.sh
+++ entry-point.sh
@@ -4,6 +4,12 @@ set -e
 
 export GCP_PROJECT="a208358-plx-ai-sandbox"
 
+export ARTIFACT_URL="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=a208358-sandbox-mlflow-server-artifact-root)"
+
+if [[ -z "${DATABASE_URL}" ]]; then # Allow overriding for local deployment
+    export DATABASE_URL="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=a208358-sandbox-mlflow-database-url)"
+fi
+
 if [[ -z "${PORT}" ]]; then
     export PORT=8080
 fi
@@ -12,15 +18,18 @@ if [[ -z "${HOST}" ]]; then
     export HOST=0.0.0.0
 fi
 
-if [[ -z "${DATABASE_URL}" ]]; then
-    export DATABASE_URL="$(python3 /app/get_secret.py --project="${GCP_PROJECT}" --secret=a208358-sandbox-mlflow-database-url)"
-fi
-
-export _MLFLOW_SERVER_ARTIFACT_ROOT="gs://a208358-euw3-sandbox-vertexai-notebooks-post-startup-stcript"
+export _MLFLOW_SERVER_ARTIFACT_ROOT="${ARTIFACT_URL}"
 export _MLFLOW_SERVER_FILE_STORE="${DATABASE_URL}"
 
 echo "_MLFLOW_SERVER_ARTIFACT_ROOT:${_MLFLOW_SERVER_ARTIFACT_ROOT}"
 echo "_MLFLOW_SERVER_FILE_STORE:${_MLFLOW_SERVER_FILE_STORE}"
 
 # Start MLflow and ngingx using supervisor
-exec gunicorn -b "${HOST}:${PORT}" -w 4 --log-level debug --access-logfile=- --error-logfile=- --log-level=debug mlflow_app:app
+exec gunicorn \
+    -b "${HOST}:${PORT}" \
+    -w 4 \
+    --log-level debug \
+    --access-logfile=- \
+    --error-logfile=- \
+    --log-level=debug \
+    mlflow_app:app
